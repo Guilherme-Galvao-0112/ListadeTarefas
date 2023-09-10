b61e1fec13aba9e110f5ad73e276438b68b073c5
PC Casa <guilhermegalvao925@gmail.com>
2023-09-10 19:09:06 -0400

    2

--- app/src/main/java/com/example/listatarefas/MainActivity.java
+++ app/src/main/java/com/example/listatarefas/MainActivity.java
@@ -3,19 +3,22 @@ package com.example.listatarefas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView ListaTarefas;
-    private Button AddTarefa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> tarefas = new ArrayList<>(
+                Arrays.asList());
+        ListView ListaTarefas = findViewById(R.id.ListaTarefas);
+        ListaTarefas.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tarefas ));
 
-        Tarefas Tarefas = new Tarefas();
     }
 }
